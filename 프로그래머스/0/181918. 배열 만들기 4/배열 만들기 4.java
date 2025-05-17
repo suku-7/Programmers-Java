d3e60426c201afebace5216f621d004a1d46793a
suku-7 <ryumon6550@gmail.com>
2025-05-17 13:20:23 +0900

    [level 0] Title: 배열 만들기 4, Time: 40.31 ms, Memory: 82.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stk = new Stack<>();
+        int i = 0;
+        
+        while(i<arr.length) {
+            if (stk.isEmpty()) {
+                stk.push(arr[i]);
+                i++;
+            } else if (stk.peek() < arr[i]) {
+                stk.push(arr[i]);
+                i++;
+            } else {
+                stk.pop();
+            }
+        }
+        int[] answer = new int[stk.size()];
+        for (int j=0; j<answer.length; j++) {
+            answer[j] = stk.get(j); 
+        }
+
+        return answer;
+    }
+}
