262ca336dd068d1e14aa50f556353dab747de9fa
suku <ryumon6550@gmail.com>
2025-05-25 20:39:21 +0900

    [level 2] Title: H-Index, Time: 1.42 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,6 +1,17 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] citations) {
         int answer = 0;
+        int n = citations.length;
+        Arrays.sort(citations);
+        
+        for (int i=0; i<citations.length; i++) {
+            if (citations[i] >= n-i) {
+                answer = n-i;
+                break;
+            }
+        }
         return answer;
     }
 }
