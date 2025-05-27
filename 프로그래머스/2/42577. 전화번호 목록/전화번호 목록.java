18e1b347ffceb793b04d47652ba846e74252f475
suku <ryumon6550@gmail.com>
2025-05-27 23:04:28 +0900

    [level 2] Title: 전화번호 목록, Time: 321.99 ms, Memory: 218 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,23 +2,19 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>();
+        HashSet<String> set = new HashSet<>();
         
-        // 모든 전화번호를 HashSet에 저장
-        for (String phone : phone_book) {
-            set.add(phone);
+        for (String p : phone_book) {
+            set.add(p);
         }
-
-        // 각 전화번호에 대해 접두어가 HashSet에 있는지 확인
-        for (String phone : phone_book) {
-            for (int i = 1; i < phone.length(); i++) {
-                String prefix = phone.substring(0, i);
+        for (String p : phone_book) {
+            for (int i=1; i<p.length(); i++) {
+                String prefix = p.substring(0, i);
                 if (set.contains(prefix)) {
-                    return false; // 접두어가 있으면 false 반환
+                    return false;
                 }
             }
         }
-
         return true;
     }
 }
