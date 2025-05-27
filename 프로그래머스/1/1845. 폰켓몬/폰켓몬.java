ecf49084c0b611c9ae8a80c14875fcbefc8de184
suku <ryumon6550@gmail.com>
2025-05-27 22:56:06 +0900

    [level 1] Title: 폰켓몬, Time: 5.37 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        HashSet<Integer> pokemons = new HashSet<>();
+        // HashSet을 이용해 중복 없는 포켓몬 종류를 저장
+        HashSet<Integer> set = new HashSet<>();
         
+        // 입력된 nums 배열의 각 숫자를 HashSet에 추가
         for (int num : nums) {
-            pokemons.add(num);
+            set.add(num); // 중복된 값은 자동으로 제거됨
         }
         
-        int answer = Math.min(pokemons.size(), nums.length/2);
-        return answer;
+        // 선택할 수 있는 포켓몬 수는 전체의 절반 (nums.length / 2)
+        // 중복을 제거한 포켓몬 종류 수(set.size())와 비교하여 더 작은 값을 반환
+        return Math.min(set.size(), nums.length / 2);
     }
 }
