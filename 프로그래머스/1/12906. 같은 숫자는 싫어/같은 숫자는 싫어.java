2576cd0c2af8c386d6bd24d97146e4c2bc0f817b
suku <ryumon6550@gmail.com>
2025-05-28 23:30:26 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 32.86 ms, Memory: 113 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -2,31 +2,23 @@ import java.util.*;
 
 public class Solution {
     public int[] solution(int []arr) {
-        // 결과를 저장할 큐 선언
         Queue<Integer> queue = new LinkedList<>();
         
-        // 이전에 본 숫자를 저장할 변수, 초기값은 배열에 나올 수 없는 값(-1)
         int prev = -1;
-
-        // 입력 배열을 순회하면서 연속된 중복을 제거
+        
         for (int num : arr) {
-            if (num != prev) { // 이전 값과 다르면 큐에 추가
+            if (num != prev) {
                 queue.add(num);
-                prev = num; // 현재 숫자를 이전 숫자로 갱신
+                prev = num;
             }
         }
-
-        // 큐의 크기만큼 정답 배열 생성
         int[] answer = new int[queue.size()];
         int i = 0;
-
-        // 큐에 저장된 값을 순서대로 꺼내서 정답 배열에 저장
+        
         while (!queue.isEmpty()) {
             answer[i] = queue.poll();
             i++;
         }
-
-        // 연속된 숫자를 제거한 배열 반환
         return answer;
     }
 }
