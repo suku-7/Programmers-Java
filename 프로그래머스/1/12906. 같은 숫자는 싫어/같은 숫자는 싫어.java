f87e3f170cef06bdabeda2676f35f4a1f392cd6d
suku <ryumon6550@gmail.com>
2025-05-30 22:02:27 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 36.90 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -6,14 +6,14 @@ public class Solution {
         int prev = -1;
         
         for (int num : arr) {
-            if ( num != prev) {
+            if (num != prev) {
                 queue.add(num);
                 prev = num;
             }
         }
         int[] answer = new int[queue.size()];
-        int i=0;
-        while(!queue.isEmpty()) {
+        int i = 0;
+        while (!queue.isEmpty()) {
             answer[i] = queue.poll();
             i++;
         }
