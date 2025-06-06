009dcc3a05bc4ecb781cb7ff0b7d352aa17d8f84
suku <ryumon6550@gmail.com>
2025-06-06 16:05:37 +0900

    [level 2] Title: 피로도, Time: 3.71 ms, Memory: 89.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,39 +1,23 @@
-import java.util.*;
-
 class Solution {
+    static boolean[] visited;
+    static int max = 0;
+    
     public int solution(int k, int[][] dungeons) {
-        int max = 0;
-        int n = dungeons.length;
-        
-        Queue<State> queue = new LinkedList<>();
-        queue.add(new State(k, new boolean[n], 0));
-        
-        while (!queue.isEmpty()) {
-            State current = queue.poll();
-            max = Math.max(max, current.depth);
-            
-            for (int i=0; i<n; i++) {
-                int minRequired = dungeons[i][0];
-                int cost = dungeons[i][1];
-                
-                if (!current.visited[i] && current.k >= minRequired) {
-                    boolean[] newVisited = Arrays.copyOf(current.visited, n);
-                    newVisited[i] = true;
-                    queue.add(new State(current.k - cost, newVisited, current.depth+1));
-                }
-            }
-        }
+        visited = new boolean[dungeons.length];
+        dfs(0, k, dungeons);
         return max;
     }
-    static class State {
-        int k;
-        boolean[] visited;
-        int depth;
-        
-        public State(int k, boolean[] visited, int depth) {
-            this.k = k;
-            this.visited = visited;
-            this.depth = depth;
+    public void dfs(int depth, int k, int[][] dungeons) {
+        max = Math.max(max, depth);
+        for (int i=0; i<dungeons.length; i++) {
+            int minRequired = dungeons[i][0];
+            int cost = dungeons[i][1];
+            
+            if (!visited[i] && k >= minRequired) {
+                visited[i] = true;
+                dfs(depth+1, k-cost, dungeons);
+                visited[i] = false;
+            }
         }
     }
 }
