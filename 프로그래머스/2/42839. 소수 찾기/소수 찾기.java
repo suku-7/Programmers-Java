80d57b3d2790f74e520300bebf5acf659977f822
suku <ryumon6550@gmail.com>
2025-06-06 17:17:37 +0900

    [level 2] Title: 소수 찾기, Time: 32.44 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -12,21 +12,21 @@ class Solution {
                 count++;
             }
         }
-        return count++;
+        return count;
     }
-    private void permutation(String prefix, String remain) {
+    public void permutation(String prefix, String remain) {
         if (!prefix.equals("")) {
             numberSet.add(Integer.parseInt(prefix));
         }
         for (int i=0; i<remain.length(); i++) {
-            permutation(prefix + remain.charAt(i), remain.substring(0, i)+remain.substring(i+1));
+            permutation(prefix+remain.charAt(i), remain.substring(0, i)+remain.substring(i+1));
         }
     }
-    private boolean isPrime(int num) {
-        if (num <2 ) return false;
+    public boolean isPrime(int num) {
+        if (num < 2) return false;
         int sqrt = (int) Math.sqrt(num);
-        for (int i=2; i<num; i++) {
-            if (num %i == 0) return false;
+        for (int i=2; i<=sqrt; i++) {
+            if (num % i == 0) return false;
         }
         return true;
     }
