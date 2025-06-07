3a0ceb81dc95bad64fb5307fe326680292d4587b
suku <ryumon6550@gmail.com>
2025-06-08 08:31:18 +0900

    [level 2] Title: 소수 찾기, Time: 21.53 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     Set<Integer> numberSet = new HashSet<>();
+    
     public int solution(String numbers) {
         permutation("", numbers);
         
@@ -25,7 +26,9 @@ class Solution {
         if (num < 2) return false;
         int sqrt = (int) Math.sqrt(num);
         for (int i=2; i<=sqrt; i++) {
-            if (num % i == 0) return false;
+            if (num % i == 0) {
+                return false;
+            }
         }
         return true;
     }
