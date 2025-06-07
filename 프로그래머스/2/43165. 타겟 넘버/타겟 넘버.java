b49199eb6ef95536e077efefbc2f106bf5b57235
suku <ryumon6550@gmail.com>
2025-06-07 13:23:39 +0900

    [level 2] Title: 타겟 넘버, Time: 4.35 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,33 +1,20 @@
 import java.util.*;
 
 class Solution {
-    private static class State {
-        public final int index;
-        public final int acc;
-        
-        State(int index, int acc) {
-            this.index = index;
-            this.acc = acc;
-        }
-    }
+    int answer = 0;
+    
     public int solution(int[] numbers, int target) {
-        Stack<State> s = new Stack<>();
-        s.push(new State(0, 0));
-        
-        int count = 0;
-        
-        while (!s.isEmpty()) {
-            State state = s.pop();
-            
-            if (state.index == numbers.length) {
-                if (state.acc == target) count++;
-                continue;
+        dfs(numbers, target, 0, 0);
+        return answer;
+    }
+    public void dfs(int[] numbers, int target, int depth, int sum) {
+        if (depth == numbers.length) {
+            if (sum == target) {
+                answer++;
             }
-            // +를 선택한 경우
-            s.push(new State(state.index+1, state.acc-numbers[state.index]));
-            // -를 선택한 경우
-            s.push(new State(state.index+1, state.acc+numbers[state.index]));
+            return;
         }
-        return count;
+        dfs(numbers, target, depth+1, sum+numbers[depth]);
+        dfs(numbers, target, depth+1, sum-numbers[depth]);
     }
 }
