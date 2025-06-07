43699526c51e819760f5542701d30db891f4f27c
suku <ryumon6550@gmail.com>
2025-06-07 21:55:53 +0900

    [level 2] Title: 소수 찾기, Time: 31.10 ms, Memory: 98.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
 class Solution {
-    HashSet<Integer> numberSet = new HashSet<>();
-    
+    Set<Integer> numberSet = new HashSet<>();
     public int solution(String numbers) {
         permutation("", numbers);
         
@@ -14,16 +13,16 @@ class Solution {
         }
         return count;
     }
-    public void permutation(String prefix, String remain) {
+    private void permutation(String prefix, String remain) {
         if (!prefix.equals("")) {
             numberSet.add(Integer.parseInt(prefix));
         }
         for (int i=0; i<remain.length(); i++) {
-            permutation(prefix+remain.charAt(i), remain.substring(0, i)+remain.substring(i+1));
+            permutation(prefix+remain.charAt(i), remain.substring(0,i)+remain.substring(i+1));
         }
     }
-    public boolean isPrime(int num) {
-        if (num <2 ) return false;
+    private boolean isPrime(int num) {
+        if (num < 2) return false;
         int sqrt = (int) Math.sqrt(num);
         for (int i=2; i<=sqrt; i++) {
             if (num % i == 0) return false;
