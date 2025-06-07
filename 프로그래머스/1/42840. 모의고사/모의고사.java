b1ef6decf1914096aa5b9432f0ba113e1ef5b7fc
suku <ryumon6550@gmail.com>
2025-06-08 08:26:03 +0900

    [level 1] Title: 모의고사, Time: 3.80 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -7,7 +7,6 @@ class Solution {
         int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
         int[] score = new int[3];
-        
         for (int i=0; i<answers.length; i++) {
             if (answers[i] == person1[i % person1.length]) score[0]++;
             if (answers[i] == person2[i % person2.length]) score[1]++;
