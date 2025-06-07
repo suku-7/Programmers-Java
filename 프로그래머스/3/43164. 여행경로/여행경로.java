42d1cb8d0eb5d27f2ff1313950d5e0e2843b0567
suku <ryumon6550@gmail.com>
2025-06-07 20:23:09 +0900

    [level 3] Title: 여행경로, Time: 0.68 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -10,24 +10,29 @@ class Solution {
         used = new boolean[n];
         
         Arrays.sort(tickets, (a, b) -> {
-            if (a[0].equals(b[0])) return a[1].compareTo(b[1]);
+            if (a[0].equals(b[0])) {
+                return a[1].compareTo(b[1]);
+            }
             return a[0].compareTo(b[0]);
         });
+        
         List<String> path = new ArrayList<>();
         path.add("ICN");
+        
         dfs("ICN", tickets, path, 0);
         
         return answer.toArray(new String[0]);
     }
-    private void dfs(String curr, String[][] tickets, List<String> path, int count) {
+    private void dfs(String current, String[][] tickets, List<String> path, int count) {
         if (found) return;
+        
         if (count == tickets.length) {
             answer = new ArrayList<>(path);
             found = true;
             return;
         }
         for (int i=0; i<tickets.length; i++) {
-            if (!used[i] && tickets[i][0].equals(curr)) {
+            if (!used[i] && tickets[i][0].equals(current)) {
                 used[i] = true;
                 path.add(tickets[i][1]);
                 dfs(tickets[i][1], tickets, path, count+1);
