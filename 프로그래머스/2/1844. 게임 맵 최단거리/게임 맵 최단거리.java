187a7b95d093c533f75ff66d446515fd278df391
suku <ryumon6550@gmail.com>
2025-06-07 21:34:06 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.98 ms, Memory: 56.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,13 +1,12 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
         
         int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
+        int[] dy = {0, 0, 1, -1};
         
         boolean[][] visited = new boolean[n][m];
         Queue<int[]> queue = new LinkedList<>();
@@ -16,16 +15,16 @@ class Solution {
         visited[0][0] = true;
         
         while (!queue.isEmpty()) {
-            int[] pos = queue.poll();
-            int x = pos[0];
-            int y = pos[1];
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
             
             for (int i=0; i<4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
                 if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if (maps[nx][ny] == 1 && !visited[nx][ny]) {
+                    if (!visited[nx][ny] && maps[nx][ny] == 1) {
                         maps[nx][ny] = maps[x][y]+1;
                         visited[nx][ny] = true;
                         queue.add(new int[]{nx, ny});
