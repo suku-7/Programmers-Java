6e26e576616361231a5db51d14c9d32a3a5326c9
suku <ryumon6550@gmail.com>
2025-06-08 09:04:49 +0900

    [level 2] Title: 모음 사전, Time: 8.23 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -2,20 +2,20 @@ import java.util.*;
 
 class Solution {
     List<String> wordList = new ArrayList<>();
-    String[] vowels = {"A", "E", "I", "O", "U"};
-    
+    final String[] vowels = {"A", "E", "I", "O", "U"};
+        
     public int solution(String word) {
         dfs("", 0);
         return wordList.indexOf(word)+1;
     }
-    public void dfs(String current, int depth) {
+    private void dfs(String current, int depth) {
         if (depth > 5) return;
         
         if (!current.isEmpty()) {
             wordList.add(current);
         }
         for (String v : vowels) {
-            dfs(current + v, depth+1);
+            dfs(current+v, depth+1);
         }
     }
 }
