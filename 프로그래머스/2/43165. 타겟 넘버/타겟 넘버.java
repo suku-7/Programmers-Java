452aff0571ed57d87c5588279a7b336a737fd7e3
suku <ryumon6550@gmail.com>
2025-06-08 16:53:19 +0900

    [level 2] Title: 타겟 넘버, Time: 10.04 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,15 +1,17 @@
 import java.util.*;
 
+
 class Solution {
     int answer = 0;
     
     public int solution(int[] numbers, int target) {
         dfs(numbers, target, 0, 0);
+        
         return answer;
     }
     private void dfs(int[] numbers, int target, int depth, int sum) {
         if (depth == numbers.length) {
-            if (sum == target) {
+            if (target == sum) {
                 answer++;
             }
             return;
