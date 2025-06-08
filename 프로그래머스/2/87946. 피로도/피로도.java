72b26dc14ac15fc31f75de7409d7cf91908de1e9
suku <ryumon6550@gmail.com>
2025-06-08 14:39:04 +0900

    [level 2] Title: 피로도, Time: 3.92 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -6,16 +6,19 @@ class Solution {
     
     public int solution(int k, int[][] dungeons) {
         visited = new boolean[dungeons.length];
+        
         dfs(0, k, dungeons);
+        
         return max;
     }
     private void dfs(int depth, int k, int[][] dungeons) {
         max = Math.max(max, depth);
+        
         for (int i=0; i<dungeons.length; i++) {
             int minRequired = dungeons[i][0];
             int cost = dungeons[i][1];
             
-            if (!visited[i] && k >= minRequired) {
+            if (!visited[i] && k>= minRequired) {
                 visited[i] = true;
                 dfs(depth+1, k-cost, dungeons);
                 visited[i] = false;
