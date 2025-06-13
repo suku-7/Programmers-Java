753606493e3c4eaf1f4d02f59fe3cdabdb4ee44e
suku <ryumon6550@gmail.com>
2025-06-13 22:47:34 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String name) {
+        int len = name.length();
+        int answer = 0;
+        
+        for (int i=0; i<len; i++) {
+            char c = name.charAt(i);
+            answer += Math.min(c - 'A', 'Z' - c + 1);
+        }
+        int move = len -1;
+        
+        for (int i=0; i<len; i++) {
+            int next = i + 1;
+            while (next < len && name.charAt(next) == 'A') {
+                next++;
+            }
+            int leftRight = i + len - next + Math.min(i, len-next);
+            move = Math.min(move, leftRight);
+        }
+        answer += move;
+        return answer;
+    }
+}
