708d9296ee7c5b17f32cfa63ab8099a02a930061
suku <ryumon6550@gmail.com>
2025-06-15 21:02:28 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 7.14 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -10,26 +10,28 @@ class Solution {
             this.duration = duration;
         }
     }
+    
     public int solution(int[][] rawJobs) {
         Job[] jobs = new Job[rawJobs.length];
         for (int i=0; i<jobs.length; i++) {
             jobs[i] = new Job(rawJobs[i][0], rawJobs[i][1]);
         }
         Arrays.sort(jobs, Comparator.comparingInt(job -> job.start));
+        Queue<Job> queue = new LinkedList<>(Arrays.asList(jobs));
+        PriorityQueue<Job> heap = new PriorityQueue<>(Comparator.comparingInt(job -> job.duration));
         
-        Queue<Job> q = new LinkedList<>(Arrays.asList(jobs));
-        PriorityQueue<Job> pq = new PriorityQueue<>(Comparator.comparingInt(job -> job.duration));
         int exec = 0;
         int time = 0;
-        while (!q.isEmpty() || !pq.isEmpty()) {
-            while (!q.isEmpty() && q.peek().start <= time) {
-                pq.add(q.poll());
+        
+        while (!queue.isEmpty() || !heap.isEmpty()) {
+            while (!queue.isEmpty() && queue.peek().start <= time) {
+                heap.add(queue.poll());
             }
-            if (pq.isEmpty()) {
-                time = q.peek().start;
+            if (heap.isEmpty()) {
+                time = queue.peek().start;
                 continue;
-            } 
-            Job job = pq.poll();
+            }
+            Job job = heap.poll();
             exec += time + job.duration - job.start;
             time += job.duration;
         }
