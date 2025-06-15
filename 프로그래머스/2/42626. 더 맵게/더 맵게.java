aaafcd734a4e36308f896997c69359cb40e6baf7
suku <ryumon6550@gmail.com>
2025-06-15 20:41:48 +0900

    [level 2] Title: 더 맵게, Time: 1683.48 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -4,14 +4,13 @@ class Solution {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> heap = new PriorityQueue<>();
         
-        // 초기 스코빌 지수들을 힙에 모두 넣는다.
         for (int s : scoville) {
             heap.add(s);
         }
         int count = 0;
         while (heap.size() >= 2 && heap.peek()<K) {
-            int first = heap.poll(); // 가장 맵지 않은 음식
-            int second = heap.poll(); // 두 번째로 맵지 않은 음식
+            int first = heap.poll();
+            int second = heap.poll();
             int mixed = first + (second*2);
             heap.add(mixed);
             count++;
