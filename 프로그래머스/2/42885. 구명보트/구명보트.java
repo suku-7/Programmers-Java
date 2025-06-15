21cd910c529234206edcfc87601af13c2144a739
suku <ryumon6550@gmail.com>
2025-06-15 20:27:26 +0900

    [level 2] Title: 구명보트, Time: 12.30 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,7 +4,7 @@ class Solution {
     public int solution(int[] people, int limit) {
         Arrays.sort(people);
         int left = 0;
-        int right = people.length -1;
+        int right = people.length-1;
         int boats = 0;
         
         while (left <= right) {
