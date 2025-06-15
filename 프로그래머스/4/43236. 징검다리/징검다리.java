abf06aaabe70456645be2278045f4b3961dc5682
suku <ryumon6550@gmail.com>
2025-06-15 21:32:24 +0900

    [level 4] Title: 징검다리, Time: 31.15 ms, Memory: 95.6 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,35 +1,35 @@
 import java.util.*;
 
 class Solution {
-    private boolean isValid(int d, int[] rocks, int n) {
-        int removed = 0;
-        int last = 0;
-        for (int rock : rocks) {
-            if (rock - last < d) {
-                removed++;
-                continue;
-            }
-            last = rock;
-        }
-        return removed <=n;
-    }
-    
     public int solution(int distance, int[] rocks, int n) {
-        rocks = Arrays.copyOf(rocks, rocks.length+1);
-        rocks[rocks.length-1] = distance;
         Arrays.sort(rocks);
         
-        int start = 1;
-        int end = distance +1;
+        int left = 1;
+        int right = distance;
+        int answer = 0;
         
-        while (end - start > 1) {
-            int d = (start+end)/2;
-            if (isValid(d, rocks, n)) {
-                start = d;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int removed = 0;
+            int prev = 0;
+            
+            for (int i=0; i<rocks.length; i++) {
+                if (rocks[i] - prev < mid) {
+                    removed++;
+                } else {
+                    prev = rocks[i];
+                }
+            }
+            if (distance - prev < mid) {
+                removed++;
+            }
+            if (removed > n) {
+                right = mid-1;
             } else {
-                end = d;
+                answer = mid;
+                left = mid+1;
             }
         }
-        return start;
+        return answer;
     }
 }
