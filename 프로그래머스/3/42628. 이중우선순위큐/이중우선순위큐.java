7069392bf45135c46c00f61807de62b94ae94da4
suku <ryumon6550@gmail.com>
2025-06-17 19:31:27 +0900

    [level 3] Title: 이중우선순위큐, Time: 47.63 ms, Memory: 111 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,25 +2,25 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        Queue<Integer> minpq = new PriorityQueue<>();
-        Queue<Integer> maxpq = new PriorityQueue<>(Collections.reverseOrder());
-
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        
         for (String operation : operations) {
             if (operation.startsWith("I ")) {
-                int n = Integer.parseInt(operation.substring(2));
-                minpq.offer(n);
-                maxpq.offer(n);
-            } else if (!minpq.isEmpty() && operation.equals("D -1")) {
-                maxpq.remove(minpq.poll());
-            } else if (!maxpq.isEmpty() && operation.equals("D 1")) {
-                minpq.remove(maxpq.poll());
+                int num = Integer.parseInt(operation.substring(2));
+                minHeap.add(num);
+                maxHeap.add(num);
+            } else if (operation.equals("D -1") && !minHeap.isEmpty()) {
+                int min = minHeap.poll();
+                maxHeap.remove(min);
+            } else if (operation.equals("D 1") && !maxHeap.isEmpty()) {
+                int max = maxHeap.poll();
+                minHeap.remove(max);
             }
         }
-
-        if (minpq.isEmpty() && maxpq.isEmpty()) {
-            return new int[]{0, 0};
+        if (minHeap.isEmpty() || maxHeap.isEmpty()) {
+            return new int[] {0, 0};
         }
-
-        return new int[]{maxpq.poll(), minpq.poll()};
+        return new int[] {maxHeap.poll(), minHeap.poll()};
     }
 }
