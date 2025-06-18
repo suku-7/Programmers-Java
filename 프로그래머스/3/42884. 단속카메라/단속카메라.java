13965aeb53502c01710d348f0e4a4af49424ca6c
suku <ryumon6550@gmail.com>
2025-06-18 22:07:38 +0900

    [level 3] Title: 단속카메라, Time: 12.09 ms, Memory: 58.3 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,15 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        int last = Integer.MIN_VALUE;
-        int count = 0;
         Arrays.sort(routes, Comparator.comparingInt(route -> route[1]));
+        
+        int count = 0;
+        int last = Integer.MIN_VALUE;
         
         for (int[] route : routes) {
             if (last >= route[0] && last <= route[1]) continue;
             
-            count++;
             last = route[1];
+            count++;
         }
         return count;
     }
