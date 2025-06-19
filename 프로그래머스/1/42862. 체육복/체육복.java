b7d0bb8e8ffb2c6b8d13808675f085c5e178406a
suku <ryumon6550@gmail.com>
2025-06-19 20:23:22 +0900

    [level 1] Title: 체육복, Time: 0.18 ms, Memory: 90.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         HashSet<Integer> lostSet = new HashSet<>();
@@ -7,15 +6,15 @@ class Solution {
         
         for (int l : lost) lostSet.add(l);
         for (int r : reserve) reserveSet.add(r);
+        
         HashSet<Integer> realLost = new HashSet<>(lostSet);
         
         for (int l : lost) {
             if (reserveSet.contains(l)) {
                 reserveSet.remove(l);
                 realLost.remove(l);
-            }    
+            }
         }
-        
         for (int r : reserveSet) {
             if (realLost.contains(r-1)) {
                 realLost.remove(r-1);
@@ -24,5 +23,5 @@ class Solution {
             }
         }
         return n - realLost.size();
-    }
+    } 
 }
