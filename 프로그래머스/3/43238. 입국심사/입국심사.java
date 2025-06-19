cfcb1b11c4412787245290c69f7e34324a0b82ef
suku <ryumon6550@gmail.com>
2025-06-19 19:40:57 +0900

    [level 3] Title: 입국심사, Time: 64.28 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,9 +1,8 @@
 import java.util.*;
 
-
 class Solution {
     public long solution(int n, int[] times) {
-        long left = 1;
+        long left = 0;
         int maxTime = 0;
         for (int time : times) {
             maxTime = Math.max(maxTime, time);
@@ -13,12 +12,13 @@ class Solution {
         
         while (left <= right) {
             long mid = (left + right) / 2;
-            long people = 0;
+            long person = 0;
             
             for (int time : times) {
-                people += mid / time;
+                person += mid / time;
             }
-            if (people >= n) {
+            
+            if (person >= n) {
                 answer = mid;
                 right = mid - 1;
             } else {
