6c899fd424ab9cb2755f1980d0d0189ff0d9b057
suku <ryumon6550@gmail.com>
2025-06-21 20:24:58 +0900

    [level 3] Title: 아이템 줍기, Time: 0.83 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
 class Solution {
-    static final int SIZE = 102;
+    static int SIZE = 102;
     static boolean[][] visited = new boolean[SIZE][SIZE];
-    static boolean[][] map = new boolean[SIZE][SIZE];
-    static int[] dx = {0, 0, 1, -1};
-    static int[] dy = {1, -1, 0, 0};
+    static boolean[][] maps = new boolean[SIZE][SIZE];
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
     
-    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {   
         for (int[] rect : rectangle) {
             int x1 = rect[0] * 2;
             int y1 = rect[1] * 2;
@@ -16,10 +16,10 @@ class Solution {
             
             for (int i=x1; i<=x2; i++) {
                 for (int j=y1; j<=y2; j++) {
-                    map[i][j] = true;
+                    maps[i][j] = true;
                 }
             }
-        }
+        }  
         for (int[] rect : rectangle) {
             int x1 = rect[0] * 2;
             int y1 = rect[1] * 2;
@@ -28,11 +28,10 @@ class Solution {
             
             for (int i=x1+1; i<x2; i++) {
                 for (int j=y1+1; j<y2; j++) {
-                    map[i][j] = false;
+                    maps[i][j] = false;
                 }
             }
         }
-        
         Queue<int[]> queue = new LinkedList<>();
         queue.add(new int[]{characterX * 2, characterY * 2, 0});
         visited[characterX * 2][characterY * 2] = true;
@@ -43,16 +42,17 @@ class Solution {
             int y = current[1];
             int dist = current[2];
             
-            if (x == itemX * 2 && y == itemY * 2) {
+            if (itemX * 2 == x && itemY * 2 == y) {
                 return dist/2;
             }
             for (int i=0; i<4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if (nx >= 0 && ny >= 0 && nx < SIZE && ny < SIZE) {
-                    if (!visited[nx][ny] && map[nx][ny]) {
+                if (nx >= 0 && nx >= 0 && nx < SIZE && ny < SIZE) {
+                    if (!visited[nx][ny] && maps[nx][ny] == true) {
                         visited[nx][ny] = true;
+                        
                         queue.add(new int[] {nx, ny, dist+1});
                     }
                 }
