5b754bd9911420500d1e05cd478978fde458b8bd
suku <ryumon6550@gmail.com>
2025-06-21 16:25:41 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 6.69 ms, Memory: 90.3 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,15 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         Arrays.sort(jobs, Comparator.comparingInt(j -> j[0]));
-        
         PriorityQueue<int[]> heap = new PriorityQueue<>(
             Comparator.comparingInt(j -> j[1])
         );
         
-        int time = 0;
         int index = 0;
-        int total = 0;
         int count = 0;
+        int time = 0;
+        int total = 0;
         
         while (count < jobs.length) {
             while (index < jobs.length && jobs[index][0] <= time) {
