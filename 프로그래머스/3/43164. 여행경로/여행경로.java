adcf776d2f2daebb03553a8219eefa7286d4e206
suku <ryumon6550@gmail.com>
2025-06-21 22:21:02 +0900

    [level 3] Title: 여행경로, Time: 0.60 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -15,14 +15,11 @@ class Solution {
             }
             return a[0].compareTo(b[0]);
         });
-        
         List<String> path = new ArrayList<>();
         path.add("ICN");
-        
         dfs("ICN", tickets, path, 0);
         
         return answer.toArray(new String[0]);
-        
     }
     private void dfs(String current, String[][] tickets, List<String> path, int count) {
         if (found) return;
@@ -31,11 +28,12 @@ class Solution {
             found = true;
             return;
         }
+        
         for (int i=0; i<tickets.length; i++) {
             if (!used[i] && tickets[i][0].equals(current)) {
                 used[i] = true;
                 path.add(tickets[i][1]);
-                dfs(tickets[i][1], tickets, path, count + 1);
+                dfs(tickets[i][1], tickets, path, count+1);
                 path.remove(path.size() - 1);
                 used[i] = false;
             }
