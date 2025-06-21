6ee3de1664069e63bbbc9db599b52af1e3fde931
suku <ryumon6550@gmail.com>
2025-06-21 15:56:28 +0900

    [level 2] Title: 조이스틱, Time: 0.06 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -5,19 +5,20 @@ class Solution {
         int len = name.length();
         int answer = 0;
         
-        for (int i=0; i<len; i++) {
-            char c = name.charAt(i);
+        for (char c : name.toCharArray()) {
             answer += Math.min(c - 'A', 'Z' - c + 1);
         }
         
         int move = len - 1;
+        
         for (int i=0; i<len; i++) {
             int next = i + 1;
             
             while (next < len && name.charAt(next) == 'A') {
                 next++;
             }
-            int leftRight = i + len - next + Math.min(i, len - next);
+            
+            int leftRight = i + len - next + Math.min(i, len-next);
             move = Math.min(move, leftRight);
         }
         answer += move;
