c63cb88226dd2561dd09cf49a6564a6b3453661f
suku <ryumon6550@gmail.com>
2025-06-21 16:57:24 +0900

    [level 3] Title: 이중우선순위큐, Time: 54.09 ms, Memory: 109 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,7 +3,9 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(
+            Collections.reverseOrder()
+        );
         
         for (String operation : operations) {
             if (operation.startsWith("I ")) {
@@ -12,15 +14,17 @@ class Solution {
                 maxHeap.add(num);
             } else if (operation.equals("D -1") && !minHeap.isEmpty()) {
                 int min = minHeap.poll();
-                maxHeap.remove(min);
+                maxHeap.remove(min);       
             } else if (operation.equals("D 1") && !maxHeap.isEmpty()) {
                 int max = maxHeap.poll();
                 minHeap.remove(max);
             }
         }
         if (minHeap.isEmpty() || maxHeap.isEmpty()) {
-            return new int[] {0, 0};
+            return new int[]{0, 0};
+        } else {
+            
+            return new int[]{maxHeap.peek(), minHeap.peek()};
         }
-        return new int[]{maxHeap.poll(), minHeap.poll()};
     }
 }
