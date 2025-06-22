ea451ac2fca256abbf48aa12fe3e4ab6e10736f8
suku <ryumon6550@gmail.com>
2025-06-22 17:40:17 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 11.16 ms, Memory: 88.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -6,6 +6,7 @@ class Solution {
     public int solution(int n, int[][] wires) {
         for (int i=0; i<wires.length; i++) {
             List<List<Integer>> graph = new ArrayList<>();
+            
             for (int j=0; j<=n; j++) {
                 graph.add(new ArrayList<>());
             }
@@ -16,19 +17,19 @@ class Solution {
                 graph.get(a).add(b);
                 graph.get(b).add(a);
             }
-            int count = bfs(graph, n, wires[i][0]);
+            boolean[] visited = new boolean[n+1];
+            int count = bfs(1, graph, visited);
             int diff = Math.abs(n - count - count);
             answer = Math.min(answer, diff);
         }
         return answer;
     }
-    private int bfs(List<List<Integer>> graph, int n, int start) {
-        boolean[] visited = new boolean[n+1];
+    private int bfs(int start, List<List<Integer>> graph, boolean[] visited) {
         Queue<Integer> queue = new LinkedList<>();
-        visited[start] = true;
         queue.add(start);
-        
+        visited[start] = true;
         int count = 1;
+        
         while (!queue.isEmpty()) {
             int current = queue.poll();
             for (int next : graph.get(current)) {
