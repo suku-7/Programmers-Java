fbf38a3263ccd2fbdc8b13c258e5721b59e1aef2
suku <ryumon6550@gmail.com>
2025-06-22 09:50:29 +0900

    [level 3] Title: 아이템 줍기, Time: 0.96 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,14 +1,13 @@
 import java.util.*;
 
-
 class Solution {
-    static int SIZE = 102;
-    static boolean[][] visited = new boolean[SIZE][SIZE];
-    static boolean[][] maps = new boolean[SIZE][SIZE];
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    int SIZE = 102;
+    boolean[][] visited = new boolean[SIZE][SIZE];
+    boolean[][] maps = new boolean[SIZE][SIZE];
+    int[] dx = {-1, 1, 0, 0};
+    int[] dy = {0, 0, -1, 1};
     
-    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {   
+    public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         for (int[] rect : rectangle) {
             int x1 = rect[0] * 2;
             int y1 = rect[1] * 2;
@@ -20,7 +19,7 @@ class Solution {
                     maps[i][j] = true;
                 }
             }
-        }  
+        }
         for (int[] rect : rectangle) {
             int x1 = rect[0] * 2;
             int y1 = rect[1] * 2;
@@ -34,7 +33,7 @@ class Solution {
             }
         }
         Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{characterX * 2, characterY * 2, 0});
+        queue.add(new int[] {characterX * 2, characterY * 2, 0});
         visited[characterX * 2][characterY * 2] = true;
         
         while (!queue.isEmpty()) {
@@ -43,9 +42,10 @@ class Solution {
             int y = current[1];
             int dist = current[2];
             
-            if (itemX * 2 == x && itemY * 2 == y) {
+            if (x == itemX * 2 && y == itemY * 2) {
                 return dist/2;
             }
+            
             for (int i=0; i<4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
@@ -53,7 +53,6 @@ class Solution {
                 if (nx >= 0 && ny >= 0 && nx < SIZE && ny < SIZE) {
                     if (!visited[nx][ny] && maps[nx][ny] == true) {
                         visited[nx][ny] = true;
-                        
                         queue.add(new int[] {nx, ny, dist+1});
                     }
                 }
