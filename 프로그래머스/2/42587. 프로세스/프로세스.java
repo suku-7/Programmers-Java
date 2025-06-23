f0527ff4590021d3f5ef5cbc1dd1ea4f416035d7
suku <ryumon6550@gmail.com>
2025-06-23 22:12:09 +0900

    [level 2] Title: 프로세스, Time: 3.59 ms, Memory: 93.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,14 +1,13 @@
 import java.util.*;
 
-
 class Solution {
     public int solution(int[] priorities, int location) {
         Queue<int[]> queue = new LinkedList<>();
-        int answer = 0;
         
         for (int i=0; i<priorities.length; i++) {
-            queue.add(new int[]{i, priorities[i]});
+            queue.add(new int[] {i, priorities[i]});
         }
+        int answer = 0;
         
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
@@ -17,15 +16,13 @@ class Solution {
             for (int[] q : queue) {
                 if (q[1] > current[1]) {
                     highP = true;
-                    break;
                 }
             }
             if (highP) {
                 queue.add(current);
             } else {
                 answer++;
-                
-                if (location == current[0]) {
+                if (current[0] == location) {
                     return answer;
                 }
             }
