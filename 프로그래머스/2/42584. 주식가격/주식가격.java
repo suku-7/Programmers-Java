9cbc6a9d2109ab59e5cf25a8650d48d8c5c33d05
suku <ryumon6550@gmail.com>
2025-06-23 22:17:48 +0900

    [level 2] Title: 주식가격, Time: 78.35 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -4,19 +4,18 @@ class Solution {
     public int[] solution(int[] prices) {
         int n = prices.length;
         int[] answer = new int[n];
-        
         Stack<Integer> stack = new Stack<>();
         
         for (int i=0; i<n; i++) {
             while (!stack.isEmpty() && prices[i] < prices[stack.peek()]) {
-                int top = stack.pop();
-                answer[top] = i-top;
+                int prev = stack.pop();
+                answer[prev] = i-prev;
             }
             stack.push(i);
         }
         while (!stack.isEmpty()) {
-            int top = stack.pop();
-            answer[top] = n - top - 1;
+            int prev = stack.pop();
+            answer[prev] = n - prev - 1;
         }
         return answer;
     }
