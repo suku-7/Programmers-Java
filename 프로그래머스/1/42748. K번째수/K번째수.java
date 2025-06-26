d082eb306b0df44036cd7475541aa42911dc197b
suku <ryumon6550@gmail.com>
2025-06-26 22:29:20 +0900

    [level 1] Title: K번째수, Time: 0.48 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,14 +3,13 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        
         for (int idx=0; idx<commands.length; idx++) {
             int i = commands[idx][0];
             int j = commands[idx][1];
             int k = commands[idx][2];
             
             int[] result = Arrays.copyOfRange(array, i-1, j);
-            Arrays.sort(result);            
+            Arrays.sort(result);
             
             answer[idx] = result[k-1];
         }
