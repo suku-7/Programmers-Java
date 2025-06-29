ad45e6ff8896b694bbe8c202ff6896538d56d5f4
suku <ryumon6550@gmail.com>
2025-06-29 09:11:22 +0900

    [level 2] Title: 의상, Time: 0.12 ms, Memory: 87.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,5 +1,4 @@
 import java.util.*;
-
 class Solution {
     public int solution(String[][] clothes) {
         HashMap<String, Integer> map = new HashMap<>();
@@ -8,12 +7,11 @@ class Solution {
             String type = item[1];
             map.put(type, map.getOrDefault(type, 0)+1);
         }
-        
         int answer = 1;
         
-        for (int num : map.values()) {
-            answer *= num+1;
+        for (int count : map.values()) {
+            answer *= (count+1);
         }
-        return answer-1;
+        return answer - 1;
     }
 }
