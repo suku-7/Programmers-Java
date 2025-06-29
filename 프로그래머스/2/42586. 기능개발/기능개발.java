427f2501489b49a525d5937f63fdcdb08768f0e5
suku <ryumon6550@gmail.com>
2025-06-29 09:19:52 +0900

    [level 2] Title: 기능개발, Time: 0.24 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -3,13 +3,12 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
         Queue<Integer> queue = new LinkedList<>();
-        
         for (int i=0; i<progresses.length; i++) {
             int remain = 100 - progresses[i];
             int day = (remain + speeds[i]-1) / speeds[i];
             queue.add(day);
         }
-        List<Integer> result = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
         
         while (!queue.isEmpty()) {
             int first = queue.poll();
@@ -19,8 +18,13 @@ class Solution {
                 queue.poll();
                 count++;
             }
-            result.add(count);
+            answer.add(count);
         }
-        return result.stream().mapToInt(i -> i).toArray();
+        int[] result = new int[answer.size()];
+        
+        for (int i=0; i<answer.size(); i++) {
+            result[i] = answer.get(i);
+        }
+        return result; 
     }
 }
