ef87fa21485261ec2d89324c5db9f682b07383ee
suku <ryumon6550@gmail.com>
2025-06-29 09:04:17 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 78.27 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,11 +1,13 @@
 import java.util.*;
 
+
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         HashMap<String, Integer> map = new HashMap<>();
         for (String p : participant) {
             map.put(p, map.getOrDefault(p, 0)+1);
-        }
+        }        
         for (String c : completion) {
             map.put(c, map.get(c)-1);
         }
