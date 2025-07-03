22a6ae17de9e4c6424d89627dd0f1b4a142639d9
suku <ryumon6550@gmail.com>
2025-07-03 23:22:42 +0900

    [level 2] Title: 더 맵게, Time: 1828.18 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -3,12 +3,11 @@ import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
         PriorityQueue<Integer> heap = new PriorityQueue<>();
-        int count = 0;
         
         for (int s : scoville) {
             heap.add(s);
         }
-        
+        int count = 0;
         while (heap.size() >= 2 && heap.peek() < K) {
             int first = heap.poll();
             int second = heap.poll();
@@ -17,7 +16,7 @@ class Solution {
             count++;
         }
         if (heap.peek() >= K) {
-            return count++;
+            return count;
         } else {
             return -1;
         }
