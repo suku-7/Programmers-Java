3aad20532600a15db237fc89d9768657cea8fdbc
suku <ryumon6550@gmail.com>
2025-07-09 22:15:08 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.95 ms, Memory: 55.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -10,6 +10,7 @@ class Solution {
         
         boolean[][] visited = new boolean[n][m];
         Queue<int[]> queue = new LinkedList<>();
+        
         visited[0][0] = true;
         queue.add(new int[]{0, 0});
         
@@ -23,7 +24,7 @@ class Solution {
                 int ny = y + dy[i];
                 
                 if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if (!visited[nx][ny] && maps[nx][ny]==1) {
+                    if (!visited[nx][ny] && maps[nx][ny] == 1) {
                         maps[nx][ny] = maps[x][y] + 1;
                         visited[nx][ny] = true;
                         queue.add(new int[]{nx, ny});
