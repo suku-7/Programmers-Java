377e0ebf48faca37c6225e4aae70d9b188da7844
suku <ryumon6550@gmail.com>
2025-07-12 21:10:24 +0900

    [level 2] Title: 전화번호 목록, Time: 357.15 ms, Memory: 248 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        Set<String> set = new HashSet<>();
+        HashSet<String> set = new HashSet<>();
         
         for (String num : phone_book) {
             set.add(num);
