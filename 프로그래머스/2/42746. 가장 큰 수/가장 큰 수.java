8f56ee91ecde7d707a258cf22c4bea3dddf50f33
suku <ryumon6550@gmail.com>
2025-07-13 16:20:20 +0900

    [level 2] Title: 가장 큰 수, Time: 190.32 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -5,8 +5,9 @@ class Solution {
         String[] nums = new String[numbers.length];
         for (int i=0; i<numbers.length; i++) {
             nums[i] = String.valueOf(numbers[i]);
-        }        
-        Arrays.sort(nums, (o1, o2) -> (o2+o1).compareTo(o1+o2));
+        }
+        Arrays.sort(nums, (o1, o2) -> (o2+o1).compareTo(o1 + o2));
+        
         if (nums[0].equals("0")) {
             return "0";
         }
