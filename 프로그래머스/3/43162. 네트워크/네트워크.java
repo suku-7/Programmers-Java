e5e5cdfd4a09a7ea8d1f7ddb697674d36e2cc3b0
suku <ryumon6550@gmail.com>
2025-07-27 14:44:14 +0900

    [level 3] Title: 네트워크, Time: 0.24 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,9 +1,9 @@
 import java.util.*;
-
 class Solution {
     public int solution(int n, int[][] computers) {
         boolean[] visited = new boolean[n];
         int answer = 0;
+        
         for (int i=0; i<n; i++) {
             if (!visited[i]) {
                 dfs(computers, visited, i, n);
