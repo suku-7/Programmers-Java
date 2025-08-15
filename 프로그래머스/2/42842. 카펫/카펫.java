749188c3b8ae35129208265b9cdefe5b63323ea7
suku <ryumon6550@gmail.com>
2025-08-15 17:36:31 +0900

    [level 2] Title: 카펫, Time: 0.04 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,11 +1,13 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
         int total = brown + yellow;
-        for (int height=3; height <=total; height++) {
-            if (total % height == 0) {
+        for (int height=3; height<=total; height++) {
+            if (total % height ==0) {
                 int width = total / height;
                 
-                if ((width - 2 ) * (height - 2) == yellow) {
+                if ((width-2) * (height-2) == yellow) {
                     return new int[]{width, height};
                 }
             }
