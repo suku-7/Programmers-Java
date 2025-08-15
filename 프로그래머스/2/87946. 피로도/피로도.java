3b6679b5a778ce60a638086ef4d89134853891ed
suku <ryumon6550@gmail.com>
2025-08-16 07:41:34 +0900

    [level 2] Title: 피로도, Time: 3.62 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,5 +1,3 @@
-import java.util.*;
-
 class Solution {
     static boolean[] visited;
     static int max = 0;
@@ -21,5 +19,5 @@ class Solution {
                 visited[i] = false;
             }
         }
-    } 
+    }
 }
