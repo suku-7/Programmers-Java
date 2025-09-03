89c509c3cb33f25bfa1a2d281d432273e7834002
suku <ryumon6550@gmail.com>
2025-09-03 21:16:50 +0900

    [level 1] Title: 모의고사, Time: 0.99 ms, Memory: 84.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -5,16 +5,17 @@ class Solution {
         int[] person1 = {1, 2, 3, 4, 5};
         int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
+
         int[] score = new int[3];
+
         for (int i=0; i<answers.length; i++) {
-            if(answers[i] == person1[i % person1.length]) score[0]++;
-            if(answers[i] == person2[i % person2.length]) score[1]++;
-            if(answers[i] == person3[i % person3.length]) score[2]++;
+            if (answers[i] == person1[i % person1.length]) score[0]++;
+            if (answers[i] == person2[i % person2.length]) score[1]++;
+            if (answers[i] == person3[i % person3.length]) score[2]++;
         }
         int maxScore = Math.max(score[0], Math.max(score[1], score[2]));
         List<Integer> result = new ArrayList<>();
-        
+
         for (int i=0; i<3; i++) {
             if (maxScore == score[i]) {
                 result.add(i+1);
